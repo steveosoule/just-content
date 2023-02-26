500d7ff83f1a3187e2c6a9713e026a199097ffef
Steven Soule <me@stevensoule.com>
2023-02-26 16:45:41 -0500

    Added netlify deployment
    https://docs.astro.build/en/guides/deploy/netlify/

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: netlify()
 });
